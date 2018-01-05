441bb9c44e621e321c4b8302dae4c24f3b5f0d68
Sparsh <skkumarsparsh@gmail.com>
2018-01-05 20:45:15 +0530

    basic client and server programs

---
+++ Windows/client.py
@@ -0,0 +1,29 @@
+import socket
+
+HOST = '172.16.31.51'
+PORT = 5000
+BUFSIZ = 1024
+
+if __name__ == '__main__':
+    client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = input("Enter hostname [%s]: " %HOST) or HOST
+    port = input("Enter port [%s]: " %PORT) or PORT
+
+    sock_addr = (host, int(port))
+    client_sock.connect(sock_addr)
+
+    payload = 'GET TIME'
+
+    try:
+        # while True:
+        client_sock.send(payload.encode('utf-8'))
+        data = client_sock.recv(BUFSIZ)
+        print(repr(data))
+            # more = input("Want to send more data to server[y/n]:")
+            # if more.lower() == 'y':
+            #     payload = input("Enter payload: ")
+            # else:
+            #     break
+    except KeyboardInterrupt:
+        print("Exited by user")
+    client_sock.close()
